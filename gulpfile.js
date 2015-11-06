5baab0bc224b077b29eb215e8472fcb9a167c635
Yoshiya Hinosawa <stibium121@gmail.com>
2015-11-06 20:59:05 +0900

    Add comment

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,16 @@ gulp.task('js', function () {
 
     return gulp.src('src/**/*.js', {base: 'src'})
 
+        // transform vinyl objects using gulp-map plugin
         .pipe(plugins.map(function (file) {
 
             gutil.log(file.path);
 
+            // replace file contents with browserify's bundle stream
             file.contents = browserify(file.path, {debug: true})
                 .transform('babelify', {presets: ['es2015']})
                 .bundle()
+                // externalize the sourcemap
                 .pipe(exorcist('dest/' + file.relative + '.map'));
 
             return file;
