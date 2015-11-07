69dade33182db4f98dfbf8e63e6d89b5fa7507c8
Yoshiya Hinosawa <stibium121@gmail.com>
2015-11-07 17:18:22 +0900

    Switch from exorcist to sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,37 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var browserify = require('browserify');
-var exorcist = require('exorcist');
 var gutil = require('gulp-util');
 
 gulp.task('js', function () {
 
     return gulp.src('src/**/*.js', {base: 'src'})
 
-        // transform vinyl objects using gulp-map plugin
+        // transform file objects using gulp-map plugin
         .pipe(plugins.map(function (file) {
 
-            gutil.log(file.path);
+            gutil.log('bundling ' + file.path);
 
             // replace file contents with browserify's bundle stream
             file.contents = browserify(file.path, {debug: true})
                 .transform('babelify', {presets: ['es2015']})
-                .bundle()
-                // externalize the sourcemap
-                .pipe(exorcist('dest/' + file.relative + '.map'));
+                .bundle();
 
             return file;
 
         }))
+
+        // transform streaming contents into buffer contents (because gulp-sourcemaps does not support streaming contents)
+        .pipe(plugins.buffer())
+
+        // load and init sourcemaps
+        .pipe(plugins.sourcemaps.init({loadMaps: true}))
+
+        // uglify
+        .pipe(plugins.uglify())
+
+        // write sourcemaps
+        .pipe(plugins.sourcemaps.write('./'))
 
         .pipe(gulp.dest('dest'));
 
